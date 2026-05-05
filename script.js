7276c076f460d5411f8d6bcbecbb7a74c3b0f6a6
barack <barackannan00@gmail.com>
2026-05-05 15:06:40 +0300

    Add car data structure and update detail links to include car selection

---
+++ script.js
@@ -0,0 +1,74 @@
+let cars = [
+  {
+    id: 1,
+    name: "BMW X5",
+    price: "Ksh 3,500,000",
+    description: "2022 . SUV . Automatic",
+    Engine:
+    Milage:
+    FuelType:
+    DriveType:
+    Color:
+    Seats:
+  },
+  {
+    id: 2,
+    name: "Toyota Prado",
+    price: "Ksh 4,200,000",
+    description: "2021 . SUV . Automatic",
+    Engine:
+    Milage:
+    FuelType:
+    DriveType:
+    Color:
+    Seats:
+  },
+
+  {id: 3,
+    name: "Mercedes Benz GLE",
+    price: "Ksh 15,700,000",
+    description: "2021 . SUV . Automatic",
+    Engine:
+    Milage:
+    FuelType:
+    DriveType:
+    Color:
+    Seats:
+  }
+
+  {id: 2,
+    name: "Toyota Prado",
+    price: "Ksh 4,200,000",
+    description: "2021 . SUV . Automatic",
+    Engine:
+    Milage:
+    FuelType:
+    DriveType:
+    Color:
+    Seats:
+  }
+
+  {id: 2,
+    name: "Toyota Prado",
+    price: "Ksh 4,200,000",
+    description: "2021 . SUV . Automatic",
+    Engine:
+    Milage:
+    FuelType:
+    DriveType:
+    Color:
+    Seats:
+  }
+
+  {id: 2,
+    name: "Toyota Prado",
+    price: "Ksh 4,200,000",
+    description: "2021 . SUV . Automatic",
+    Engine:
+    Milage:
+    FuelType:
+    DriveType:
+    Color:
+    Seats:
+  }
+];
